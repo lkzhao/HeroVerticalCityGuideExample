e16cdaf0ba7a86f42ad0fa5303df922873fc0ccb
Luke Zhao <lzhaoyilun@gmail.com>
2017-02-05 15:20:51 -0500

    snapchat like sliding present transition

--- HeroVerticalCityGuideExample/FirstViewController.swift
+++ HeroVerticalCityGuideExample/FirstViewController.swift
@@ -28,7 +28,7 @@ class FirstViewController: UIViewController {
   var cities = City.cities
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     if let currentCell = sender as? CityCell,
-       let vc = segue.destination as? SecondViewController {
+      let vc = segue.destination as? SecondViewController {
       vc.city = currentCell.city
     }
   }
@@ -43,42 +43,52 @@ class FirstViewController: UIViewController {
   }
 
   func handlePan(gestureRecognizer:UIPanGestureRecognizer) {
+    // calculate the progress based on how far the user moved
+    let translation = panGR.translation(in: nil)
+    let progress = -translation.x / view.bounds.width
+
     switch panGR.state {
     case .began:
       // begin the transition when sliding left
       if panGR.velocity(in: nil).x < 0,
-         let indexPath = collectionView.indexPathForItem(at: panGR.location(in: collectionView)) {
-        let vc = self.storyboard!.instantiateViewController(withIdentifier: "second") as! SecondViewController
+        let indexPath = collectionView.indexPathForItem(at: panGR.location(in: collectionView)) {
+        let toVC = self.storyboard!.instantiateViewController(withIdentifier: "second") as! SecondViewController
+        toVC.city = cities[indexPath.item]
+
         slidingCell = collectionView.cellForItem(at: indexPath) as! CityCell?
         slidingCell!.imageView.heroID = nil
         slidingCell!.nameLabel.heroID = nil
         slidingCell!.descriptionLabel.heroID = nil
-        slidingCell!.heroModifiers = [.translate(x:-view.bounds.width)]
-
-        vc.city = cities[indexPath.item]
-        vc.view.heroModifiers = [.translate(x:view.bounds.width)]
+        slidingCell!.heroModifiers = [.translate(x: -100)]
+        view.heroModifiers = [.translate(x:-view.bounds.width)]
+        toVC.view.heroModifiers = [.translate(x:view.bounds.width)]
 
         // begin the transition as normal
-        present(vc, animated: true, completion: nil)
+        present(toVC, animated: true, completion: nil)
       }
     case .changed:
       if let slidingCell = slidingCell, let toVC = Hero.shared.toViewController as? SecondViewController {
-        // calculate the progress based on how far the user moved
-        let translation = panGR.translation(in: nil)
-        let progress = -translation.x / view.bounds.width
         Hero.shared.update(progress: Double(progress))
 
         // update views' position (limited to only vertical scroll)
-        Hero.shared.apply(modifiers: [.translate(x:translation.x)], to: slidingCell)
-        Hero.shared.apply(modifiers: [.translate(x:translation.x + view.bounds.width)], to: toVC.view)
+        let cellTranslationX = translation.x < -100 ? -100 : translation.x
+        let wholeViewTranslationX = translation.x < -100 ? translation.x + 100 : 0
+        Hero.shared.apply(modifiers: [.translate(x:cellTranslationX)], to: slidingCell)
+        Hero.shared.apply(modifiers: [.translate(x:wholeViewTranslationX)], to: view)
+        Hero.shared.apply(modifiers: [.translate(x:wholeViewTranslationX + view.bounds.width)], to: toVC.view)
       }
     default:
       if let slidingCell = slidingCell, let toVC = Hero.shared.toViewController as? SecondViewController {
         slidingCell.reset() // reset heroModifers for the slidingCell
         toVC.view.heroModifiers = nil
+        view.heroModifiers = nil
 
-        // end the transition when user ended their touch
-        Hero.shared.end()
+        // end or cancel the transition based on the progress and user's touch velocity
+        if progress + -panGR.velocity(in: nil).x / view.bounds.width > 0.3 {
+          Hero.shared.end()
+        } else {
+          Hero.shared.cancel()
+        }
       }
     }
   }
